7dda80d6b12c9665229acf028f683ddd0c902551
BaptisteManusset <Baptiste.Manusset@gmail.com>
2021-02-13 18:10:56 +0100

    update visual

---
+++ Assets/OffsetScrolling.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class OffsetScrolling : MonoBehaviour
+{
+  public float scrollSpeed;
+
+  private Renderer renderer;
+  private Vector2 savedOffset;
+
+  void Start()
+  {
+    renderer = GetComponent<Renderer>();
+  }
+
+  void Update()
+  {
+    float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+    Vector2 offset = new Vector2(x, x);
+    renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+  }
+}
